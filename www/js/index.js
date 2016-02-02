d9dda55d8d647ceab1b4414878a3e96ff9fa49f8
Ashotyanski <ashotk0510@gmail.com>
2016-02-02 20:10:01 +0500

    full caching with Imgcache.js added, initial parts positions added, first start bug fixed

--- www/js/index.js
+++ www/js/index.js
@@ -33,17 +33,61 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function () {
-        var jTransfer = new FileTransfer();
-        jTransfer.download('http://pastebin.com/raw/cKPcFcpJ', cordova.file.dataDirectory + 'levels.json',
-            function (entry) {
-                var s = entry.nativeURL;
-                alert('json загружен');
-            },
-            function (error) {
-                alert('json лох, код ошибки:  ' + error.code);
-            },
-            true
-        );
+        ImgCache.init();
+        window.resolveLocalFileSystemURL(cordova.file.dataDirectory+'levels.json', function(){alert('данные загружены')},function(){
+            var jTransfer = new FileTransfer();
+            jTransfer.download('http://pastebin.com/raw/cKPcFcpJ', cordova.file.dataDirectory + 'levels.json',
+                function (entry) {
+                    var s = entry.nativeURL;
+                    alert('данные загружены');
+
+                    window.resolveLocalFileSystemURL(cordova.file.dataDirectory + 'levels.json',
+                        function (fileEntry) {
+                            fileEntry.file(function (file) {
+                                var reader = new FileReader();
+                                reader.onloadend = function (e) {
+
+                                    var level = JSON.parse(this.result);
+                                    //if (level) { alert('объект уровня null, что-то случилось при чтении файла'); page('/index');}
+
+                                    level.forEach(function(element,index) {
+                                        alert('index: '+index+', id: '+element.id);
+                                        element.parts.forEach(function(element) {
+                                            ImgCache.isCached(element.ref, function (path, success) {
+                                                if (success) {
+                                                    console.log('already cached')
+                                                }
+                                                else {
+                                                    ImgCache.cacheFile(element.ref,
+                                                        function () {
+                                                            console.log('cached');
+                                                        },
+                                                        function () {
+                                                            console.log('cache problem');
+                                                        }
+                                                    );
+                                                }
+                                            });
+                                        })
+                                    });
+                                };
+                                reader.readAsText(file);
+                            })
+                        },
+                        function (e) {
+                            alert("ошибка файловой системы: " + e);
+                            page('/index');
+                        }
+                    );
+                },
+                function (error) {
+                    alert('данные не загрузились, дальнейшая работа невозможна, код ошибки:  ' + error.code);
+                },
+                true
+            );
+        });
+
+
         page.base('');
         page('/index', index);
         page('/play/:id', play);
@@ -59,29 +103,43 @@ var app = {
 // match any of the following routes
 //
 function index() {
-    var sector = document.getElementById('sector');
-    sector.innerHTML = '';
-    for(var i = 0; i<2; i++) {
-        sector.innerHTML += '<div id="start-button"><a href="/play/' + i + '">Уровень '+i+'</a></div>';
-    }
     /*
-     var fileTransfer = new FileTransfer();
-     fileTransfer.download('http://freepacman.ru/images/play-game.png', cordova.file.cacheDirectory + 'start.png',
-     function (entry) {
-     var s = entry.nativeURL;
-     alert('картинка загружена, она теперь должна быть на кнопке');
-     sector.innerHTML = '<div id="start-button"><a href="/play"><img src="' + s.substr(8) + '"></a></div>';
+     var db = openDatabase('Levels','1.0','Levels',10000);
+     db.transaction(function (tx) {
+     tx.executeSql('CREATE TABLE IF NOT EXISTS lv_data (id INTEGER PRIMARY KEY, level_id INTEGER, parts TEXT )');
+     //tx.executeSql("INSERT INTO lv_data (level_id, parts) VALUES (1,'maybe bullshit?' ) ");
+     alert('inserted');
+     });
+     db.transaction(function (tx) {
+     tx.executeSql('SELECT * FROM lv_data', [], function (tx, results) {
+     var len = results.rows.length, i;
+     for (i = 0; i < len; i++){
+     alert(results.rows.item(i).parts);
+     }
+     });
+     });
+     */
+    var app_section = document.getElementById('app_section');
+    app_section.innerHTML = '';
+    for(var i = 0; i<2; i++) {
+        app_section.innerHTML += '<div id="start-button"><a href="/play/' + i + '">Уровень '+i+'</a></div>';
+    }
+/*
+    var fileTransfer = new FileTransfer();
+    fileTransfer.download('http://freepacman.ru/images/play-game.png', cordova.file.cacheDirectory + 'start.png',
+        function (entry) {
+            var s = entry.nativeURL;
+            app_section.innerHTML += '<img src="' + s.substr(8) + '">';
 
-     },
-     function (error) {
-     alert('картинка не загрузилась, на кнопке обычный текст (можно взять из кэша)\nКод ошибки:  ' + error.code);
-     sector.innerHTML = '<div id="start-button"><a href="/play"><h1>Играть</h1></a></div>';
-     },
-     true
-     );
+        },
+        function (error) {
+            alert('картинка не загрузилась, на кнопке обычный текст (можно взять из кэша)\nКод ошибки:  ' + error.code);
+            //app_section.innerHTML = '<div id="start-button"><a href="/play"><h1>Играть</h1></a></div>';
+        },
+        true
+    );
 
-     /*
-     window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function(dir) {
+    window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function(dir) {
      dir.getFile("levels.json", {create:true}, function(file) {
      file.createWriter( function(fileWriter) {
      //fileWriter.seek(fileWriter.length);
@@ -101,8 +159,8 @@ function play(ctx) {
     var done = 0;
 
 
-    var sector = document.getElementById('sector');
-    sector.innerHTML = '';
+    var app_section = document.getElementById('app_section');
+    app_section.innerHTML = '';
 
     window.resolveLocalFileSystemURL(cordova.file.dataDirectory + 'levels.json',
         function (fileEntry) {
@@ -117,63 +175,124 @@ function play(ctx) {
         },
         function (e) {
             alert("ошибка файловой системы: " + e);
+            page('/index');
         }
     );
 
     function parseLevel(lv) {
+        if (!lv) { alert('объект уровня null, что-то случилось при чтении файла'); page('/index');}
 
-
-        if (lv) {
-            sector.innerHTML += '<div class="parts" id="main" ><img src="' + lv[lid].main.ref + '" ></div>';
-            lv[lid].parts.forEach(function (element) {
-                sector.innerHTML += '<div class="draggable parts" id="' + element.id + '" ><img src="' + element.ref + '" ></div>';
+        app_section.innerHTML += '<div class="parts" id="main" ><img src="' + lv[lid].main.ref + '" ></div>';
+        lv[lid].parts.forEach(function (element) {
+            ImgCache.isCached(element.ref, function(path, success) {
+                if(success) {
+                    app_section.innerHTML += '<div class="draggable parts" id="' + element.id + '" >' +
+                        '<img src="' + cordova.file.cacheDirectory + ImgCache.private.getCachedFilePath(element.ref) + '"></div>';
+                }
+                else {
+                    ImgCache.cacheFile(element.ref,
+                        function () {
+                            app_section.innerHTML += '<div class="draggable parts" id="' + element.id + '" >' +
+                                '<img src="' + cordova.file.cacheDirectory + ImgCache.private.getCachedFilePath(element.ref) + '"></div>';
+                            //alert('done '+ImgCache.private.getCachedFileFullPath(element.ref));
+                        },
+                        function () {
+                            alert('cache problem')
+                        }
+                    );
+                }
             });
+        });
+        setTimeout(function () {
 
-            setTimeout(function () {
+            var main = document.getElementById('main');
 
-                var main = document.getElementById('main');
+            var santaw = main.offsetWidth / window.devicePixelRatio;
+            var santah = main.offsetHeight / window.devicePixelRatio;
 
-                var santaw = main.offsetWidth / window.devicePixelRatio;
-                var santah = main.offsetHeight / window.devicePixelRatio;
+            main.setAttribute('style', 'top:' + ((screen.height / 2) - santah / 2) + 'px' +
+                '; left:' + ((screen.width / 2) - santaw / 2) + 'px');
+            var santax = main.offsetLeft;
+            var santay = main.offsetTop;
+            $('#app_section').css('display', 'block');
+            $('.parts img').css({
+                width: function (index, value) {
+                    return parseFloat(value) / window.devicePixelRatio;
+                }
+            });
+            $('.draggable').draggable();
 
-                main.setAttribute('style', 'top:' + ((screen.height / 2) - santah / 2) + 'px' +
-                    '; left:' + ((screen.width / 2) - santaw / 2) + 'px');
-                var santax = main.offsetLeft;
-                var santay = main.offsetTop;
-                $('#section').css('display', 'block');
-                $('.parts img').css({
-                    width: function (index, value) {
-                        return parseFloat(value) / window.devicePixelRatio;
+
+            lv[lid].parts.forEach(function (element,index) {
+                var top_bar = Math.floor(lv[lid].parts.length/2)-1;
+                var bottom_bar = lv[lid].parts.length - top_bar;
+
+                var iniy,inix;
+                if(index<=top_bar){
+                    $('#'+element.id).css('position', 'absolute');
+                    inix = (index)*screen.width/(top_bar+1)+(parseFloat($('#'+element.id).css('width'))/window.devicePixelRatio)/2;
+                    $('#'+element.id).css('left', inix );
+                    iniy = santay/2 - (parseFloat($('#'+element.id).css('height'))/window.devicePixelRatio)/2;
+                    $('#'+element.id).css('top', iniy );
+                }
+                else {
+                    $('#'+element.id).css('position', 'absolute');
+                    inix = (index-top_bar)*screen.width/bottom_bar-(parseFloat($('#'+element.id).css('width'))/window.devicePixelRatio)/2;
+                    $('#'+element.id).css('left', inix );
+                    iniy =santay + santah+(screen.height- santay - santah)/2 - (parseFloat($('#'+element.id).css('height'))/window.devicePixelRatio)/2;
+                    $('#'+element.id).css('top', iniy );
+                }
+
+                /*
+                var sector = 360/lv[lid].parts.length;
+                var angle = 0;
+                var radius = screen.width/4;
+                angle +=sector;
+                var tan = Math.tan(angle * Math.PI/180);
+                var inix = Math.sqrt(radius*radius/(tan+1));
+                var iniy = inix*tan;
+                alert('sector: ${sector} \n angle: ${angle} \n tan: ${tan} \n inix: ${inix} \n iniy: ${iniy}');
+                $('#'+element.id).css('position', 'absolute');
+                $('#'+element.id).css('top', iniy);
+                $('#'+element.id).css('left', inix);
+                */
+                $('#'+element.id).on('mouseup', function(e){
+
+                    var x = santax + (element.x / window.devicePixelRatio);
+                    var y = santay + (element.y / window.devicePixelRatio);
+                    if (e.target.x > (x - 100) && e.target.x < (x + 100) && e.target.y > (y - 100) && e.target.y < (y + 100)) {
+                        $('#'+element.id).css('position', 'absolute');
+                        $('#'+element.id).css('top', y);
+                        $('#'+element.id).css('left', x);
+                        $('#'+element.id).off('mouseup');
+                        $('#'+element.id).draggable('destroy');
+                        check(lv[lid].parts.length);
+                    }
+                    else {
+                        if(index<=top_bar){
+                            $('#'+element.id).css('position', 'absolute');
+                            $('#'+element.id).css('left', inix );
+                            $('#'+element.id).css('top', iniy );
+                        }
+                        else {
+                            $('#'+element.id).css('position', 'absolute');
+                            $('#'+element.id).css('left', inix );
+                            $('#'+element.id).css('top', iniy );
+                        }
+
                     }
                 });
-                $('.draggable').draggable();
+            });
+        }, 1000);
 
 
-                lv[lid].parts.forEach(function (element) {
-                    $('#'+element.id).on('mouseup', function(e){
-                        var x = santax + (element.x / window.devicePixelRatio);
-                        var y = santay + (element.y / window.devicePixelRatio);
-                        if (e.target.x > (x - 100) && e.target.x < (x + 100) && e.target.y > (y - 100) && e.target.y < (y + 100)) {
-                            $('#'+element.id).css('position', 'absolute');
-                            $('#'+element.id).css('top', y);
-                            $('#'+element.id).css('left', x);
-                            $('#'+element.id).off('mouseup');
-                            $('#'+element.id).draggable('destroy');
-                            check(lv[lid].parts.length);
-                        }
-                    })
-                });
-            }, 300);
-
-        }
-        else alert('объект уровня null, что-то случилось при чтении файла');
     }
     function check(n) {
         done++;
         if (done == n) {
             done = 0;
             $('<div id="win">Уровень пройден!<p><a href="/index" >Вернуться в главное меню</a></p></div>')
-                .appendTo('#sector');
+                .appendTo('#app_section');
             var win = document.getElementById('win');
             var winh = win.offsetHeight;
             var winw = win.offsetWidth;
